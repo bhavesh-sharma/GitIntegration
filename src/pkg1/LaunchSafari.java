8d40d2d44d97c521ddc003f459a5307fd168fd90
bhavesh-sharma <bhavesh_sharma@yahoo.com>
2019-07-20 22:10:04 +0530

    First round of commit----

---
+++ src/pkg1/LaunchSafari.java
@@ -0,0 +1,11 @@
+package pkg1;
+
+public class LaunchSafari {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Launch Safari Browser");
+
+	}
+
+}
---
+++ src/pkg2/LaunchChrome.java
@@ -0,0 +1,11 @@
+package pkg2;
+
+public class LaunchChrome {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Launh Chrome Browser");
+
+	}
+
+}
